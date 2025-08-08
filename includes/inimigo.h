43a7b0f832ddc4e94b579a189e72762dd69d1ed6
João Pedro Lima de Araujo <jpla@cin.ufpe.br>
2025-08-08 17:02:57 -0300

    Update inimigo.h

--- includes/inimigo.h
+++ includes/inimigo.h
@@ -1,13 +1,15 @@
 #ifndef INIMIGOS_H
 #define INIMIGOS_H
 
-
-
-
-
-
-
-
+typedef struct {
+    Vector2 posicao;
+    int dano;
+    float alcance;
+    float cooldown;
+    int custo;
+    bool ativo;
+    int velocidade;
+} Inimigo;
 
 
 
