0a1a30b44e2f7c601aa9b9be15382ee733399cea
André Polo Norte <acpnf@cin.ufpe.br>
2025-08-09 20:15:28 -0300

    Create nivel_2.c

---
+++ includes/nivel_2.c
@@ -0,0 +1,31 @@
+#include "nivel_2.h"
+#include "menu.h"
+#include <string.h>
+
+Nivel2 IniciarNivel2() {
+    Nivel2 nivel2 = {0};
+    
+    // Mapa do nível 2
+    int mapa[10][20] = {
+        {1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+        {0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+        {0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1}
+    };
+
+    memcpy(nivel2.mapa, mapa, sizeof(mapa));
+    nivel2.vida_jogador = 500;
+    nivel2.moedas_iniciais = 100;
+    
+    // Carrega texturas (substitua pelos caminhos reais)
+    nivel2.textura_caminho = LoadTexture("jogo/imagens/caminho_brick.png");
+    nivel2.textura_grama = LoadTexture("jogo/imagens/reck.png");
+
+    return nivel2;
+}
